d02723a83c87c5a35be04c89d43c15d17196fc86
Kittipos <ki11ip0.s.a.s@gmail.com>
2023-04-15 21:32:09 +0700

    Add icon & change app name to AWC

--- dev/dev-install.py
+++ dev/dev-install.py
@@ -4,7 +4,7 @@ import subprocess
 from sys import platform
 from pathlib import Path
 
-app_name = "AdrenalWashout"
+app_name = "AWC"
 
 
 
@@ -21,8 +21,8 @@ if platform == "darwin":
     print("On Mac")
     # Run on Mac
     cmd_mac_str = f"""
-pyinstaller --noconfirm --onedir -n '{app_name}_macos' --windowed --add-data "{home_dir}/opt/miniconda3/lib/python3.9/site-packages/customtkinter:customtkinter/" app.py
-tar -czf dist/{app_name}_macos.tar.gz dist/{app_name}_macos.app
+pyinstaller --noconfirm --onedir -n '{app_name}' --windowed --add-data "{home_dir}/opt/miniconda3/lib/python3.9/site-packages/customtkinter:customtkinter/" --icon="assets/app.ico" app.py
+tar -czf dist/{app_name}.tar.gz dist/{app_name}.app
 """
     subprocess.run(cmd_mac_str, shell=True)
     # print(cmd_mac_str)
@@ -30,7 +30,7 @@ else:
     print("On Windows")
     # Run on Win
     cmd_win_str = fr"""
-pyinstaller --noconfirm --onefile -n {app_name}_win --windowed --add-data "{home_dir}\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages\\customtkinter;customtkinter/" app.py
+pyinstaller --noconfirm --onefile -n {app_name} --windowed --add-data "{home_dir}\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages\\customtkinter;customtkinter/" app.py
 """ 
     os.system(cmd_win_str)
     #subprocess.run(cmd_win_str, shell=True) # Not work
