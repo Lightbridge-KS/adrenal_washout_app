82f98e028506b868de62dbce5256d9f06e6a709c
Kittipos <ki11ip0.s.a.s@gmail.com>
2023-04-15 20:54:57 +0700

    not hard coded home Dir

--- dev/dev-install.py
+++ dev/dev-install.py
@@ -1,7 +1,10 @@
 # Run This Script from Project Root (Dev Only)
 import os
-from sys import platform
 import subprocess
+from sys import platform
+from pathlib import Path
+
+app_name = "AdrenalWashout"
 
 
 
@@ -11,22 +14,23 @@ os.chdir("src")
 
 print(os.getcwd())
 
-app_name = "AdrenalWashout"
+home_dir = str(Path.home()) # User Home Directory
 
 
 if platform == "darwin":
     print("On Mac")
     # Run on Mac
     cmd_mac_str = f"""
-pyinstaller --noconfirm --onedir -n '{app_name}_macos' --windowed --add-data "/Users/kittipos/opt/miniconda3/lib/python3.9/site-packages/customtkinter:customtkinter/" app.py
+pyinstaller --noconfirm --onedir -n '{app_name}_macos' --windowed --add-data "{home_dir}/opt/miniconda3/lib/python3.9/site-packages/customtkinter:customtkinter/" app.py
 tar -czf dist/{app_name}_macos.tar.gz dist/{app_name}_macos.app
 """
     subprocess.run(cmd_mac_str, shell=True)
+    # print(cmd_mac_str)
 else:
     print("On Windows")
     # Run on Win
     cmd_win_str = fr"""
-pyinstaller --noconfirm --onefile -n {app_name}_win --windowed --add-data "C:\\Users\\prach\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages\\customtkinter;customtkinter/" app.py
+pyinstaller --noconfirm --onefile -n {app_name}_win --windowed --add-data "{home_dir}\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages\\customtkinter;customtkinter/" app.py
 """ 
     os.system(cmd_win_str)
     #subprocess.run(cmd_win_str, shell=True) # Not work
